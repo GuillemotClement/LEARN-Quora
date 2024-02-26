7d285af15d26d892c996a7c188ca7789a33ce9c5
GuillemotClement <guillemot.clement@protonmail.com>
2024-02-26 18:47:43 +0100

    🎨 Mise en place du premier controller (design et héritage du base.html.twig)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,8 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     //controle qui gère l'entrée de l'application
+    //premier argument correspond au l'adresse url. Ici ce sera la premiere page de l'appli
+    //name correspond au nom de la route. Permet de se référé à la route en utilisant son nom
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
